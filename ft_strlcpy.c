1f8a754a18f6a251483bd10b316ec8e79a24d2b9
Dani <danixidev@gmail.com>
2024-01-10 16:54:17 +0100

    Corregir norminette strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:53:37 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/10 15:57:24 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/10 16:52:50 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if(n == 0)
+	if (n == 0)
 		return (ft_strlen(src));
 	while (src[i] != '\0' && i < (n - 1))
 	{
