26d1fc453226bb17d6fe7011374eb9b2e3bbbc7d
Dani <danixidev@gmail.com>
2024-01-10 18:26:15 +0100

    Crear strlcat

--- Makefile
+++ Makefile
@@ -11,6 +11,7 @@ SRCS = ft_isalpha.c \
        ft_memset.c \
        ft_memmove.c \
        ft_strlcpy.c \
+       ft_strlcat.c \
        ft_bzero.c \
        ft_memcpy.c \
        ft_toupper.c \
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:55:59 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/09 18:28:40 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/10 16:48:00 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-//strlcat
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 //strchr
