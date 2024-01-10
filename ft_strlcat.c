126d80b080851a936f45705c60be4b84ba94edaf
Daniel Ximenez <danixidev@gmail.com>
2024-01-10 19:18:15 +0100

    Corregir strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:45:37 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/10 18:36:55 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/10 19:16:56 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
 	size_t	s;
 	size_t	d;
+	size_t	r;
 	size_t	i;
 
-	s = ft_strlen(src);
 	d = ft_strlen(dest);
+	s = ft_strlen(src);
 	i = 0;
-	while (src[i] != '\0' && n < (d + 1))
+	if (n > d)
+		r = s + d;
+	else
+		r = s + n;
+	while (src[i] && n > (d + 1))
 	{
 		dest[d] = src[i];
-		s++;
 		d++;
+		i++;
 	}
 	dest[d] = '\0';
-	if(n < d)
-		return (s + d);
-	else
-		return (s + n);
+	return (r);
 }
 
 /*
