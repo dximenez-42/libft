be487b4654b1abe55600fc2a7aa0e0bc981c73b0
Daniel Ximenez <danixidev@gmail.com>
2024-01-16 14:24:42 +0100

    Revisar strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:43:59 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/09 14:40:33 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/16 14:24:15 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ char	*ft_strdup(const char *s)
 	char	*mem;
 	size_t	i;
 
-	mem = malloc(sizeof(s));
+	mem = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (mem == 0)
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
 	{
 		mem[i] = s[i];
-		i++;
+		++i;
 	}
+	mem[i] = '\0';
 	return (mem);
 }
 
