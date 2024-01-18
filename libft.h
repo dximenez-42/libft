23a7974819c5550556fbe1e37ecb97e6bd5afbae
Daniel Ximenez <danixidev@gmail.com>
2024-01-18 21:31:45 +0100

    Resolver itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:55:59 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/17 19:59:37 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/18 18:21:48 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-// char	*ft_itoa(int n);
+char	*ft_itoa(int n);
 // char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 // void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 // void	ft_putchar_fd(char c, int fd);
