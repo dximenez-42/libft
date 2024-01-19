5d93a574fe7d92951c8b17c88ac49d07334bd03c
Daniel Ximenez <danixidev@gmail.com>
2024-01-19 13:24:59 +0100

    Resolver putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 13:14:30 by dximenez          #+#    #+#             */
+/*   Updated: 2024/01/19 13:24:25 by dximenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	write_num(int n, int fd)
+{
+	char	c;
+
+	c = '0' + n;
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n *= -1;
+		}
+		if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			n %= 10;
+		}
+		if (n < 10)
+			write_num(n, fd);
+	}
+}
