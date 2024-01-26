1c3adf187eb8d2ed1fafc020c6d65e45642f4c77
Daniel Ximenez <danixidev@gmail.com>
2024-01-26 14:57:54 +0100

    Resolver split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 19:59:00 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/26 14:22:13 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/26 14:57:38 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,11 @@
 char	*allocate_word(const char *s, size_t end)
 {
 	char	*mem;
-	size_t	i;
 
 	mem = ft_calloc((end + 1), sizeof(char));
 	if (mem == 0)
 		return (0);
-	i = 0;
-	while (i < end)
-	{
-		mem[i] = s[i];
-		++i;
-	}
-	mem[i] = '\0';
+	ft_strlcpy(mem, s, end + 1);
 	return (mem);
 }
 
@@ -47,18 +40,24 @@ char	**free_all(char	**mem, size_t size)
 size_t	count_words(char const *s, char del)
 {
 	size_t	c;
+	size_t	flag;
 
-	if (ft_strlen(s) == 0)
-		return (0);
 	c = 0;
-	s = ft_strtrim(s, &del);
+	flag = 0;
+	if (s == 0)
+		return (0);
 	while (*s != '\0')
 	{
 		if (*s == del)
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
 			++c;
+		}
 		++s;
 	}
-	return (c + 1);
+	return (c);
 }
 
 size_t	word_size(char const *s, char c)
@@ -77,23 +76,19 @@ char	**ft_split(char const *s, char c)
 	size_t	size;
 	size_t	i;
 
-	s = ft_strtrim(s, &c);
 	mem = ft_calloc(count_words(s, c) + 1, sizeof(char *));
-	// printf("allocated %zu words\n\n", count_words(s, c));	//
 	if (mem == 0)
 		return (0);
 	i = 0;
-	while (count_words(s, c) > 0 && *s != '\0')
+	while (count_words(s, c) > 0)
 	{
-		// printf("remaining words: %zu\n", count_words(s, c));	//
 		while (*s == c)
 			++s;
 		size = word_size(s, c);
 		mem[i] = allocate_word(s, size);
 		if (mem[i] == 0)
 			return (free_all(mem, count_words(s, c) + i));
-		// printf("-> '%s'\n", mem[i]);	//
-		s += size + 1;
+		s = ft_strchr(s, (int)c);
 		++i;
 	}
 	return (mem);
@@ -122,7 +117,7 @@ char	**ft_split(char const *s, char c)
 
 // int	main(void)
 // {
-// 	char *str = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
+// 	char *str = "lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 // 	char sep = ' ';
 // 	char **s = ft_split(str, sep);
 // }
