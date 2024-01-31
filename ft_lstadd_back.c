681e696fdde359aa759c378e22b5f2464f6ea619
Daniel Ximenez <danixidev@gmail.com>
2024-01-31 18:04:29 +0100

    Hacer mains lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 21:09:22 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/20 21:21:31 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/31 18:03:22 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,15 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	last = ft_lstlast(*lst);
 	last->next = new;
 }
+
+/*
+int main()
+{
+	t_list *list = ft_lstnew("test1");
+	list->next = ft_lstnew("test2");
+
+	t_list *new = ft_lstnew("new");
+	ft_lstadd_back(&list, new);
+	printf("%d\n", ft_lstsize(list));
+}
+*/
