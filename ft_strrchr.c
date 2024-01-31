4ecb5c9a7005fa774deb74644e0ebae52e7f3bb4
Daniel Ximenez <danixidev@gmail.com>
2024-01-31 19:20:44 +0100

    Main strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 13:30:14 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/26 16:50:42 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/31 19:20:26 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,27 @@ char	*ft_strrchr(const char *s, int ch)
 	return (0);
 }
 
-// int main()
-// {
-// 	const char *str = "hola que tal";
-// 	char *res = ft_strrchr(str, 's');
-// 	printf("%s\n", res);		// NULL
-// }
+/*
+int main()
+{
+	const char *str = "hola que tal";
+	char *res = ft_strrchr(str, 's');
+	printf("%s\n", res);		// NULL
+}
+*/
 
-// int main()
-// {
-// 	char *str = "hola que tal";
-// 	printf("%s\n", ft_strrchr(str + 2, 'h'));		// NULL
-// }
+/*
+int main()
+{
+	char *str = "hola que tal";
+	printf("%s\n", ft_strrchr(str + 2, 'h'));		// NULL
+}
+*/
 
-// int main()
-// {
-// 	char *str = "";
-// 	printf("%s\n", ft_strrchr(str, '\0'));		// \0
-// }
+/*
+int main()
+{
+	char *str = "";
+	printf("%s\n", ft_strrchr(str, '\0'));		// \0
+}
+*/
