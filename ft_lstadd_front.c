f30e22d117d4a317ee3f0710ab57da57e701e328
Daniel Ximenez <danixidev@gmail.com>
2024-01-31 18:06:13 +0100

    Mains lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:33:44 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/26 14:24:47 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/31 18:06:05 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,15 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	lst[0] = new;
 	new->next = temp;
 }
+
+/*
+int main()
+{
+	t_list *list = ft_lstnew("test1");
+	list->next = ft_lstnew("test2");
+
+	t_list *new = ft_lstnew("new");
+	ft_lstadd_front(&list, new);
+	printf("%d\n", ft_lstsize(list));
+}
+*/
