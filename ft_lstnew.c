e240de760c12fafff3ef649db3c9dc9eaf380062
Daniel Ximenez <danixidev@gmail.com>
2024-01-31 18:15:42 +0100

    Main lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 17:34:44 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/19 18:32:23 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/31 18:15:27 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,13 @@ t_list	*ft_lstnew(void *content)
 	list->next = 0;
 	return (list);
 }
+
+/*
+int main()
+{
+	t_list *list = ft_lstnew("test1");
+
+	printf("%d\n", (char *)list->content);
+	printf("%p\n", list->next);
+}
+*/
