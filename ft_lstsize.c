d67c1d7ede5bcaed647b31ad5c4917ddcd209897
Daniel Ximenez <danixidev@gmail.com>
2024-01-31 18:17:27 +0100

    Main lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:47:18 by dximenez          #+#    #+#             */
-/*   Updated: 2024/01/26 14:24:33 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/01/31 18:17:14 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,11 @@ int	ft_lstsize(t_list *lst)
 /*
 int main()
 {
-	t_list	element3;
-	t_list	element2;
-	t_list	element1;
+	t_list *list = ft_lstnew("test1");
+	list->next = ft_lstnew("test2");
 
-	element3.content = "3";
-	element3.next = 0;
-	
-	element2.content = "2";
-	element2.next = &element3;
-	
-	element1.content = "1";
-	element1.next = &element2;
-
-	printf("%d\n", ft_lstsize(&element1));
+	t_list *new = ft_lstnew("new");
+	ft_lstadd_back(&list, new);
+	printf("%d\n", ft_lstsize(list));
 }
 */
